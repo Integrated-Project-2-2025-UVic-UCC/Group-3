0bf34f6258854dfa418aef1d18229326865b40f7
Alvar05 <92118932+Alvar05@users.noreply.github.com>
2025-03-24 15:34:22 +0100

    Add files via upload

---
+++ src/Code/ESC_DRON.ino
@@ -0,0 +1,26 @@
+#include<Servo.h>
+
+Servo ESC;
+
+int vel = 1000; // Amplitud minima de pulso para tu ESC
+
+void setup() {
+  ESC.attach(9); // Pin en el que funciona
+  //Activar el ESC
+  ESC.writeMicroseconds(1000); //1000 = 1ms
+  //Cambia el 1000 anterior por 2000 si
+  //tu ESC se activa con un pulso de 2ms
+  delay(5000); //Esperar 5 segundos para hacer la activacion
+  Serial.begin(9600);
+  Serial.setTimeout(10);
+}
+void loop() {
+ if(Serial.available() >= 1)
+  {
+    vel = Serial.parseInt(); //Leer un entero por serial
+    if(vel != 0)
+    {
+      ESC.writeMicroseconds(vel); //Generar un pulso con el numero recibido
+    }
+  }
+}
